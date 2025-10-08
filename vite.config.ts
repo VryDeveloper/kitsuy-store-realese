63a96649bae4162d41af2d4f992cd43bdbac6110
VryDeveloper <victor.092.augusto@gmail.com>
2025-10-08 20:34:52 -0300

    att-3.0

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "./", // ← ADICIONE ESTA LINHA
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
