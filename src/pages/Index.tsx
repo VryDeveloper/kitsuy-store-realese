a69d03750eb9359bc2159f33995d6466da2265af
VryDeveloper <victor.092.augusto@gmail.com>
2025-12-27 21:06:44 -0300

    verdetalhesbttn

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -497,6 +497,7 @@ const Index = () => {
 
                     <button
                       className="w-full px-4 py-2 rounded-xl bg-primary text-white font-semibold hover:opacity-90 mt-auto"
+                      onClick={() => window.open(whatsappLink, '_blank')}
                     >
                       Ver Detalhes
                     </button>
